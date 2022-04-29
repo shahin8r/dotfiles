0a19852039d3f63d8073c9fed2c565cfb5ab921b
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2022-04-29 23:45:45 +0200

    Pause dunst notifications when i3lock

--- bin/i3-lock-screen.sh
+++ bin/i3-lock-screen.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+dunst set-paused true
+
 img=$(mktemp /tmp/XXXXXXXXXX.png)
 import -window root $img 
 convert $img -scale 10% -scale 1000% $img
-i3lock -u -i $img
+
+i3lock -n -u -i $img
 rm $img
+
+dunst set-paused false
