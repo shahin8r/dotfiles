ce23d431466e4f833c8ac001e09e1b4af3ae0558
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2022-04-29 23:47:20 +0200

    Show notification of keyboard layout when switching

--- bin/toggle_keyboard_layout.sh
+++ bin/toggle_keyboard_layout.sh
@@ -4,8 +4,10 @@ KBD_LAYOUT=$(setxkbmap -query | grep layout | sed -e 's/layout://g' | sed -e 's/
 
 if [[ $KBD_LAYOUT == "us" ]]; then
   setxkbmap se
+  echo 'SE'
 else
   setxkbmap us
+  echo 'US'
 fi
 
 setxkbmap -option ctrl:nocaps
