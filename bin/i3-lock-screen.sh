a9f664a6be622349765abbfb802228cfeeda6aff
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2022-10-03 21:24:26 +0200

    Update i3lock script and use xidlehook instead

--- bin/i3-lock-screen.sh
+++ bin/i3-lock-screen.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-if [ -f "/tmp/screen_awake" ]; then
-  exit 0
-fi
-
-if [ -z "$XSS_SLEEP_LOCK_FD" ]; then
-  notify-send 'Locking screen in 15s'
-  sleep 15s
-
-  if [ -f "/tmp/screen_awake" ]; then
-    exit 0
-  fi
-fi
-
 dunst set-paused true
 
 img=$(mktemp /tmp/XXXXXXXXXX.png)
-import -window root $img 
+import -window root $img
 convert $img -scale 10% -scale 1000% $img
 
 i3lock -n -u -i $img
