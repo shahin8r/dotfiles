ae8c28d867827427e8fe4b302455d7585d371cb7
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2022-10-14 20:20:45 +0200

    Tweak UI colors

--- nvim-lua/lua/theme.lua
+++ nvim-lua/lua/theme.lua
@@ -3,8 +3,11 @@ vim.api.nvim_set_hl(0, 'Normal', { bg = 'none', fg = '#eeeeee' })
 vim.api.nvim_set_hl(0, 'NonText', {bg = 'none', fg = '#666666' })
 vim.api.nvim_set_hl(0, 'LineNr', { bg = 'none', fg = '#666666' })
 vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#101010' })
-vim.api.nvim_set_hl(0, 'StatusLine', { bg = '#222222', fg = '#cccccc' })
+vim.api.nvim_set_hl(0, 'StatusLine', { bg = '#181818', fg = '#cccccc' })
 vim.api.nvim_set_hl(0, 'StatusLineNc', { bg = '#111111', fg = '#666666' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#222222' })
+vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'VertSplit', { bg = 'none', fg = '#222222' })
 
 -- statusline
 vim.opt.statusline=" %{pathshorten(expand('%:f'))} %([%{FugitiveHead()}]%) %m%r%y%w%=C:%c L:%l/%L "
