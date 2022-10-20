dfef0c143c755f62fa7769cfff333ed3551a641d
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2022-10-20 15:19:20 +0200

    Show hidden files and use vertical layout in telescope

--- nvim-lua/lua/plugins/configs/telescope.lua
+++ nvim-lua/lua/plugins/configs/telescope.lua
@@ -1,5 +1,13 @@
 local telescope = require('telescope')
 
 telescope.setup({
-  color_devicons = true
+  color_devicons = true,
+  defaults = {
+    layout_strategy = 'vertical',
+  },
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  }
 })
