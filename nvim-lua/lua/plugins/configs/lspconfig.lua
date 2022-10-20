f9329477d7addc0abceec8d8215b820f95f9d047
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2022-10-20 18:04:07 +0200

    Fix lsp for php and phpdoc for treesitter

--- nvim-lua/lua/plugins/configs/lspconfig.lua
+++ nvim-lua/lua/plugins/configs/lspconfig.lua
@@ -48,24 +48,8 @@ lsp.tsserver.setup({
 
 lsp.sumneko_lua.setup({
   on_attach = mappings.lspconfig.on_attach,
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    }
-  }
+})
+
+lsp.intelephense.setup({
+  on_attach = mappings.lspconfig.on_attach,
 })
--- nvim-lua/lua/plugins/configs/treesitter.lua
+++ nvim-lua/lua/plugins/configs/treesitter.lua
@@ -3,6 +3,7 @@ require('nvim-treesitter.configs').setup{
     'lua',
     'javascript',
     'php',
+    'phpdoc',
     'typescript',
     'hcl',
     'prisma',
