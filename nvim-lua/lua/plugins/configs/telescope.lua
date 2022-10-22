2f67b1046609a6472ae07a4119c84bbbe945b83d
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2022-10-22 10:15:18 +0200

    Ignore some files in telescope

--- nvim-lua/lua/plugins/configs/telescope.lua
+++ nvim-lua/lua/plugins/configs/telescope.lua
@@ -4,6 +4,15 @@ telescope.setup({
   color_devicons = true,
   defaults = {
     layout_strategy = 'vertical',
+    file_ignore_patterns = {
+      '.git',
+      '.angular',
+      'node_modules',
+      'dist',
+      'build',
+      'package-lock.json',
+      'yarn.lock',
+    },
   },
   pickers = {
     find_files = {
