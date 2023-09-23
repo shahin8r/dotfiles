426a6a141a9e02968041df19de101b96f3774016
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2023-09-23 11:26:06 +0200

    Add borders in LSP UI

--- nvim-lua/lua/theme.lua
+++ nvim-lua/lua/theme.lua
@@ -5,3 +5,9 @@ vim.opt.statusline = " %([%{FugitiveHead()}]%) %r%y%w %(%m %)%{pathshorten(expan
 
 -- gitgutter
 vim.g.gitgutter_sign_priority = 20
+
+-- lsp
+require('lspconfig.ui.windows').default_options.border = 'single'
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "rounded",
+})
