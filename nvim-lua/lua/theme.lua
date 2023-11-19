dcb025d7c5fe30f50149d36f67edd4b116bfb5d1
Shahin Agha-Ghassem <shahin.ag@gmail.com>
2023-11-19 16:51:20 +0100

    Change style of floating windows in neovim

--- nvim-lua/lua/theme.lua
+++ nvim-lua/lua/theme.lua
@@ -1,8 +1,12 @@
 vim.cmd [[colorscheme github_dark_high_contrast]]
 
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#0d0d0d' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { fg = '#151515' })
+vim.api.nvim_set_hl(0, 'StatusLine', { bg = '#151515' })
+vim.api.nvim_set_hl(0, 'TelescopeNormal', { bg = '#0d0d0d' })
+vim.api.nvim_set_hl(0, 'HarpoonWindow', { bg = '#0d0d0d' })
+vim.api.nvim_set_hl(0, 'HarpoonBorder', { bg = '#0d0d0d' })
 
 -- statusline
 vim.opt.statusline = " %([%{FugitiveHead()}]%) %r%y%w %(%m %)%{pathshorten(expand('%:f'))} %=C:%c L:%l/%L "
